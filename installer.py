4b937ea639d0bb92d79452396ef42a7fabf16e03
User0332 <carlzfurtado@gmail.com>
2023-05-19 21:37:37 -0400

    stage

--- installer.py
+++ installer.py
@@ -2,30 +2,47 @@ import os
 import subprocess
 import msvcrt
 import sys
+import stat
 import shutil
 
-RELEASE_BUILD = True if len(sys.argv > 1) and (sys.argv[0] == "debug") else False
+try:
+	RELEASE_BUILD = False if (len(sys.argv) > 1 and (sys.argv[1] == "debug")) else True
 
-print("installing application")
+	print("installing application")
 
-# TODO: Replace the request below and just clone the whole git repo, and then extract the desired build file
+	# TODO: Replace the request below and just clone the whole git repo, and then extract the desired build file
 
-subprocess.call(["git", "clone", "https://raw.githubusercontent.com/User0332/Regis-Desktop"])
+	if os.path.exists("Regis Desktop"): shutil.rmtree("Regis Desktop", ignore_errors=True)
 
-path_to_installation = f"build/{'release' if RELEASE_BUILD else 'debug-test'}/latest/Regis Desktop"
+	if os.path.exists("Regis-Desktop"):
+		os.chmod("Regis-Desktop", stat.S_IRWXO)
+		try: shutil.rmtree("Regis-Desktop")
+		except PermissionError:
+			print("Please run this installer as administrator!")
+			exit(1)
 
-os.rename(path_to_installation, "Regis Desktop")
-shutil.rmtree("Regis-Desktop") # this is the repo
 
-print("making installation directory")
+	subprocess.call(["git", "clone", "https://github.com/User0332/Regis-Desktop"])
 
-os.chdir("Regis Desktop")
+	path_to_installation = f"build/{'release' if RELEASE_BUILD else 'debug-test'}/latest/Regis Desktop"
 
-print("creating cache")
-os.mkdir("cache")
-print("done creating cache")
+	os.rename(f"Regis-Desktop/{path_to_installation}", "Regis Desktop")
+	
+	os.chmod("Regis-Desktop", stat.S_IRWXO)
+	shutil.rmtree("Regis-Desktop", ignore_errors=True) # this is the repo
 
-print("done installing regis desktop")
+	print("making installation directory")
 
-print("press any key to continue...")
-msvcrt.getch()
+	os.chdir("Regis Desktop")
+
+	print("creating cache")
+	os.mkdir("cache")
+	print("done creating cache")
+
+	print("done installing regis desktop")
+
+	print("press any key to continue...")
+	msvcrt.getch()
+except Exception as e:
+	print(f"error: {e}")
+	msvcrt.getch()
