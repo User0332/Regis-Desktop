45736710b69d939ac7533a54bf1f6aaf7eda9be0
User0332 <carlzfurtado@gmail.com>
2023-09-14 17:48:20 -0400

    update device id-ing method

--- is_same_device.py
+++ is_same_device.py
@@ -1,9 +1,21 @@
-import uuid
+import subprocess
 import os
 
 x, y, r, t = "12", ''.join((chr(76), chr(54), chr(52), chr(80))), "144", "ab9dn"
 
 pwd = y+y+"102mmd"+x+r+t
+
+def get_uuid():
+	if os.name == "nt": return subprocess.check_output(
+		["wmic", "csproduct", "get", "uuid"]
+	).decode().splitlines()[1]
+		
+	## only other platform this will be running on is macos
+
+	return subprocess.check_output(
+		"ioreg -d2 -c IOPlatformExpertDevice | awk -F\\\" '/IOPlatformUUID/{print $(NF-1)}'",
+		shell=True
+	)
 
 def init():
 	if not os.path.exists(f"is-same-device:{pwd}"):
@@ -14,11 +26,11 @@ def init():
 
 		if not contents: return True
 
-		if contents != str(uuid.getnode()):
+		if contents != get_uuid():
 			raise RuntimeError("Application moved to unknown device")
 
 	return False
 
 if init():
 	with open(f"is-same-device:{pwd}", 'w') as f:
-		f.write(str(uuid.getnode()))
+		f.write(get_uuid())
