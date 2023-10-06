3c04f3d185b7f34ebe690ba2e60903bc07726090
User0332 <carlzfurtado@gmail.com>
2023-10-06 08:42:53 -0400

    update mac shortcut creation

--- installutils/regis-desktop-0.0.1-installer.py
+++ installutils/regis-desktop-0.0.1-installer.py
@@ -374,16 +374,87 @@ def install():
 		wscript.Description = "Regis Desktop Launcher Shortcut"
 		wscript.IconLocation = os.path.abspath("assets/regis-icon.ico")
 		wscript.save()
-		
-	else: ## mac
-		pyshortcuts.make_shortcut(
-			script=os.path.abspath("regis-desktop.py"),
-			name="Regis Desktop",
-			description="Regis Desktop Launcher Shortcut",
-			icon=("assets/regis-icon.ico" if os.name == "nt" else "assets/regis-icon.icns"),
-			working_dir=inner,
-			terminal=False
+	else:
+		userfolders = pyshortcuts.darwin.get_folders()
+		if working_dir is None:
+			working_dir = ''
+
+		scut = pyshortcuts.shortcut("regis-desktop.py", userfolders, name="Regis Desktop", description="Regis Desktop Launcher Shortcut",
+			working_dir=working_dir, icon="assets/regis-icon.icns")
+
+		osascript = '"%s" %s' % (scut.full_script, scut.arguments)
+		osascript = osascript.replace(' ', '\\ ')
+		prefix = os.path.normpath(sys.prefix)
+		if executable is None:
+			executable = pyshortcuts.darwin.get_pyexe()
+
+		executable = os.path.normpath(executable)
+		if os.path.realpath(scut.full_script) == os.path.realpath(executable):
+			executable = ''
+
+		if not os.path.exists(scut.desktop_dir):
+			os.makedirs(scut.desktop_dir)
+
+		dest = os.path.join(scut.desktop_dir, scut.target)
+
+		if os.path.exists(dest):
+			shutil.rmtree(dest)
+
+		os.mkdir(dest)
+		os.mkdir(os.path.join(dest, 'Contents'))
+		os.mkdir(os.path.join(dest, 'Contents', 'MacOS'))
+		os.mkdir(os.path.join(dest, 'Contents', 'Resources'))
+
+		opts = dict(
+			name=scut.name,
+			desc=scut.description,
+			script=scut.full_script,
+			workdir=scut.working_dir,
+			args=scut.arguments,
+			prefix=prefix,
+			exe=executable,
+			osascript=osascript
 		)
+
+		info = """<?xml version="1.0" encoding="UTF-8"?>
+	<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
+	"http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+	<plist version="1.0">
+	<dict>
+	<key>CFBundleGetInfoString</key> <string>{desc:s}</string>
+	<key>CFBundleName</key> <string>{name:s}</string>
+	<key>CFBundleExecutable</key> <string>{name:s}</string>
+	<key>CFBundleIconFile</key> <string>{name:s}</string>
+	<key>CFBundlePackageType</key> <string>APPL</string>
+	</dict>
+	</plist>
+	"""
+
+		text = ['#!/bin/bash',
+				"export EXE={exe:s}",
+				"export SCRIPT={script:s}",
+				"export ARGS='{args:s}'"]
+
+		if scut.working_dir not in (None, ''):
+			text.append("cd {workdir:s}")
+
+			text.append("$EXE $SCRIPT $ARGS")
+
+		text.append('\n')
+		text = '\n'.join(text)
+
+		with open(os.path.join(dest, 'Contents', 'Info.plist'), 'w') as fout:
+			fout.write(info.format(**opts))
+
+		ascript_name = os.path.join(dest, 'Contents', 'MacOS', scut.name)
+		with open(ascript_name, 'w') as fout:
+			fout.write(text.format(**opts))
+
+		os.chmod(ascript_name, 493)  # = octal 755 / rwxr-xr-x
+		icon_dest = os.path.join(dest, 'Contents', 'Resources', scut.name + '.icns')
+		shutil.copy(scut.icon, icon_dest)
+
+
 
 	leave.destroy()
 
